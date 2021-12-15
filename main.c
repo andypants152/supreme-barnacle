b5f1ce8e59247f6a9213c07757f3b241250e411b
AndyPants <andypants152@gmail.com>
2021-12-15 10:19:03 -0800

    hello world

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <gb/gb.h>
+#include <gbdk/font.h>
+
+void main(){
+    font_t min_font;
+    font_init();
+    //ibm_font = font_load(font_ibm);  /* 96 tiles */
+    //italic_font = font_load(font_italic);   /* 93 tiles */
+    min_font = font_load(font_min); // 36 tile
+    font_set(min_font);
+    printf("Hello World!");
+    
+}
