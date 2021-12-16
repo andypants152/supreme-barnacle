295c410b676ae5081838d5e41587b5a2f8ae9fbf
AndyPants <andypants152@gmail.com>
2021-12-16 10:53:38 -0800

    night-time adventure

--- main.c
+++ main.c
@@ -4,9 +4,18 @@
 
 typedef enum {
     HELLO,
+    SMILES,
     SPLASH
 
 } screen_t;
+
+void waiting(uint8_t numloops){
+    uint8_t i;
+    for(i = 0; i < numloops; i++){
+        //and wait that many screen draws till the cpu is needed
+        wait_vbl_done();
+    }     
+}
 
 screen_t Hello(){
     font_t min_font;
@@ -15,10 +24,19 @@ screen_t Hello(){
     //italic_font = font_load(font_italic);   /* 93 tiles */
     min_font = font_load(font_min); // 36 tile
     font_set(min_font);
-    printf("\nHello World  \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n PRESS START TO\n Swap Phrase");
+    printf("\nHello World  \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n PRESS START TO\n Continue");
+    //TODO: swap out printf for tile placement
     waitpad(J_START);
-    return SPLASH;
+    return SMILES;
     
+}
+
+screen_t Smiles(){
+    printf("\n \n \n \n \n \n \n \n add chose your \n own adventure here \n \n \n \n \n \n \n \n \n");
+
+    waitpad(J_START);
+
+    return SPLASH;
 }
 
 screen_t Splash(){
@@ -28,17 +46,25 @@ screen_t Splash(){
     //italic_font = font_load(font_italic);   /* 93 tiles */
     min_font = font_load(font_min); // 36 tile
     font_set(min_font);
-    printf("\nBonne Nuit \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n PRESS START TO\n Swap Phrase");
+    printf("\nBonne Nuit \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n PRESS START TO\n Restart");
+    //TODO: swap out printf for tile placement
     waitpad(J_START);
     return HELLO;
 }
 
+
+
+
 void main(){
     screen_t current_screen = HELLO;
+
     
     while(1){
         if (current_screen == HELLO){
             current_screen = Hello();
+        }
+        else if (current_screen == SMILES){
+            current_screen = Smiles();
         }
         else if (current_screen == SPLASH){
             current_screen = Splash();
